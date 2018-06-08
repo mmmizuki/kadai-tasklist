3e1f71314b8d517b90cd2973a7b7a58a30f0e1ef
EC2 Default User <ec2-user@ip-172-31-36-109.us-east-2.compute.internal>
2018-06-08 06:23:34 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -96,12 +96,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request,[
-            'title' => 'required|max:191',
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
             ]);
         
         $task = Task::find($id);
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
