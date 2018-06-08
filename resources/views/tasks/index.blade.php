02cf4a9a7ea3c17b2b62fd3dc2fb5f51c639ed15
EC2 Default User <ec2-user@ip-172-31-36-109.us-east-2.compute.internal>
2018-06-08 03:22:15 +0000

    kadai saiteishutsu

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,7 +10,7 @@
                 <li>{!! link_to_route('tasks.show',$task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
             @endforeach
         </ul>
-        {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
     @endif
+    {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
 
 @endsection
