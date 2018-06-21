034b5daa8d23eb0b6943881fcc9a4cf796443539
EC2 Default User <ec2-user@ip-172-31-36-109.us-east-2.compute.internal>
2018-06-21 01:58:19 +0000

    top page

--- database/migrations/2018_06_07_125012_create_tasks_table.php
+++ database/migrations/2018_06_07_125012_create_tasks_table.php
@@ -6,17 +6,16 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTasksTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
             $table->string('content');
             $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
